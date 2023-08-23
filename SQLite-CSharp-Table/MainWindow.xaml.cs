e8cf7900fdaa97a902f516d849a1e8724e0f4645
X1atle <myron.ryzleitsev@gmail.com>
2023-08-24 01:54:59 +0200

    UI created with DataGrid some functionality
    Changes Made:
    - Created a WPF MainWindow class with a DataGrid for displaying items.
    - Added ObservableCollection<Item> to hold the item data and bind it to the DataGrid.
    - Implemented InsertButton_Click method to handle adding new items to the ObservableCollection and refreshing the DataGrid display.
    - Defined a simple Item class with Name and Id properties to represent individual items.
    - Designed the MainWindow.xaml layout with input fields for Name and Id, along with buttons for Insert, Update, and Delete actions.
    - Implement Update and Delete functionality for the items.
    - Enhance error handling and user feedback.

--- SQLite-CSharp-Table/MainWindow.xaml.cs
+++ SQLite-CSharp-Table/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,51 @@ namespace SQLite_CSharp_Table
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Item> Items { get; set; } = new ObservableCollection<Item>();
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            DataGridTable.ItemsSource = Items;
+            
+            DataGridTextColumn nameColumn = new DataGridTextColumn();
+            nameColumn.Header = "Name";
+            nameColumn.Binding = new Binding("Name");
+
+            DataGridTextColumn idColumn = new DataGridTextColumn();
+            idColumn.Header = "Id";
+            idColumn.Binding = new Binding("Id");
+        }
+        private void InsertButton_Click(object sender, RoutedEventArgs e)
+        {   
+            try
+            {
+                string name = NameTextBox.Text;
+                int id = int.Parse(IdTextBox.Text);
+                Items.Add(new Item { Name = name, Id = id });
+
+                NameTextBox.Text = "";
+                IdTextBox.Text = "";
+                DataGridTable.Items.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        { 
+            
+        }
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        { 
+            
+        }
+        public class Item
+        {
+            public string Name { get; set; }
+            public int Id { get; set; }
         }
     }
 }
